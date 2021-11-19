7cc8345bfbbac0eeac9048917d66688769b5be3e
Bianca Cervantes <cervbianca9810@gmail.com>
2021-11-18 20:07:18 -0800

    __init__

---
+++ __init__.py
@@ -0,0 +1,20 @@
+import flask
+import os
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+studyapp_obj = flask.Flask(__name__)
+studyapp_obj.config.from_mapping (
+        SECRET_KEY = 'Milo',
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir,'app.db'),
+        SQLALCHEMY_TRACK_MODIFICATIONS = False 
+        )
+
+db=SQLAlchemy(studyapp_obj)
+
+login=LoginManager(studyapp_obj)
+login.login_view='login'
+
+from studyapp import routes,models
