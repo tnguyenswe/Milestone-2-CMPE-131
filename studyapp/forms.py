900ae3bc9435c749cc19a65db517c85b928013ab
Taran <taransinghchowdhury@gmail.com>
2021-11-29 15:56:07 -0800

    added feature-search text in file

--- studyapp/forms.py
+++ studyapp/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField
+from flask_wtf.file import FileField, FileRequired
 from wtforms import StringField,BooleanField,PasswordField,SubmitField
 from wtforms.validators import DataRequired
 
@@ -8,7 +8,7 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
-    
+
 class SignupForm(FlaskForm):
     username=StringField("Username",validators=[DataRequired()])
     password=PasswordField("Password",validators=[DataRequired()])
@@ -20,3 +20,8 @@ class UploadForm(FlaskForm):
 class ToDoForm(FlaskForm):
     todo=StringField('Start building your to-do list!',validators=[DataRequired()])
     submit=SubmitField()
+
+class SearchTextForm(FlaskForm):
+    input_file = FileField('Upload', validators=[FileRequired()])
+    text = StringField('Search Text', validators=[DataRequired()])
+    search = SubmitField('Search')
