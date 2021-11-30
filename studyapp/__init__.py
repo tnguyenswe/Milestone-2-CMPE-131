8682be0c97411b067f1e81e07d7ddfb7a31dbb9b
Taran <taransinghchowdhury@gmail.com>
2021-11-29 16:07:37 -0800

    fix for search Text in file feature

--- studyapp/__init__.py
+++ studyapp/__init__.py
@@ -5,11 +5,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+UPLOAD_FOLDER = 'studyapp/static/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'md'}
+
 studyapp_obj = flask.Flask(__name__)
+
+studyapp_obj.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 studyapp_obj.config.from_mapping (
         SECRET_KEY = 'Milo',
         SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir,'app.db'),
-        SQLALCHEMY_TRACK_MODIFICATIONS = False 
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
         )
 
 db=SQLAlchemy(studyapp_obj)
